1d0bbf82402309b2ccc58d2975b634f7e3bb592a
Emdad5353 <myselfemdad@gmail.com>
2023-03-19 16:43:58 +0000

    describe and nested describe

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,15 +1,28 @@
 import {render, screen} from '@testing-library/react'
 import {Greet} from './greet';
 
-test(
-    'Greet renders correctly', ()=>{
-        render(<Greet />)
-        const textElement= screen.getByText('Hello');
+describe('Grret', ()=>{
+    test.skip(
+        'Greet renders correctly', ()=>{
+            render(<Greet />)
+            const textElement= screen.getByText('Hello');
+            expect(textElement).toBeInTheDocument()
+        }
+    ) 
+
+    describe('Grret', ()=>{
+        test.skip(
+            'Greet renders correctly', ()=>{
+                render(<Greet />)
+                const textElement= screen.getByText('Hello');
+                expect(textElement).toBeInTheDocument()
+            }
+        ) 
+    })
+    
+    test.only('Greet renders with a name', ()=>{
+        render(<Greet name='Emdad'/>)
+        const textElement= screen.getByText('Hello Emdad')
         expect(textElement).toBeInTheDocument()
-    }
-) 
-test('Greet renders with a name', ()=>{
-    render(<Greet name='Emdad'/>)
-    const textElement= screen.getByText('Hello Emdad')
-    expect(textElement).toBeInTheDocument()
+    })
 })
